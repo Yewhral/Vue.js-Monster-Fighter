b443cec607e3e4426a94d964a815583180c315c6
Yewhral <zabrzyjewski@gmail.com>
2017-07-25 15:35:05 +0200

    Logic added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Vue.js-Monster-Fighter
 
-Project summarizing my knowledge about Vue so far
+Project summarizing my knowledge about Vue so far.
+
+Very simple one, but it was surprisingly refreshing and good for checking if I understand Vue so far. And hm, a confidence boost is always nice :D I can use Vue for something cool now!
--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
-/**
- * Created by Doethe on 2017-07-25.
- */
+new Vue({
+   el: '#vue-app',
+   data:{
+       health: 100,
+       ended: false
+   },
+   methods:{
+       punch:function(number) {
+           this.health -= number;
+           if(this.health <= 0){
+               this.ended = true;
+           }
+       },
+       restart:function() {
+           this.health = 100;
+           this.ended = false;
+       }
+   },
+   computed:{
+
+   }
+});
